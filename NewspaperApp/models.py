0d49040cb87386199f41ec73ca0d3c6bedc7d382
Irfan Ullah <theirfanullah@gmail.com>
2018-08-23 03:00:16 -0700

    Articles are integrated with Postresql DB

--- NewspaperApp/models.py
+++ NewspaperApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ArticleModel(models.Model):
+	article_id = models.AutoField(primary_key=True)
+	publish_date = models.CharField(max_length=40,null=True)
+	visit_date = models.CharField(max_length=40)
+	article_url = models.TextField()
+
+	class Meta:
+		db_table = "articleTable"
--- NewspaperApp/views.py
+++ NewspaperApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import newspaper
+from NewspaperApp.models import ArticleModel
 from newspaper import Article
+import datetime
 import json
 # Create your views here.
 def index(request):
@@ -14,5 +16,22 @@ def detail(request):
 	query = request.GET.get("u")
 	article = Article(url=query)
 	article.download()
-	article.parse()
-	return render(request, 'post.html',{'article' : article})
+	if article.download_state == 1:
+	    return HttpResponse("Article can not be downloaded. Possible Reason: Invalid URL.")
+	elif article.download_state == 0:
+	    return HttpResponse("Article can not be downloaded. Possible Reason: Invalid URL.")
+	else:
+		isExist = ArticleModel.objects.filter(article_url=query)
+		if len(isExist) == 0:
+			article.parse()
+			obj = ArticleModel(
+				publish_date = article.publish_date,
+				visit_date = datetime.datetime.now(),
+				article_url = article.url
+				)
+
+			obj.save()
+		else:
+			article.parse()
+
+		return render(request, 'post.html', {'article' : article})
